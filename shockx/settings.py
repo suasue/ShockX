2f42cd6be4653299ac57a0c0eef2ed52e3baa049
suasue <devsu7477@gmail.com>
2021-05-11 17:09:39 +0900

    Add: docker-compose.yml 추가

--- shockx/settings.py
+++ shockx/settings.py
@@ -140,7 +140,7 @@ APPEND_SLASH = False
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
